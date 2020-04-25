c419d3e4033d1053e94144535a08b0388e84b59c
Ryan Harrison <ryan.harrison@student.manchester.ac.uk>
2020-04-25 19:25:45 +0100

    Send message from one user to another mediated by bot

--- server/src/bot.ts
+++ server/src/bot.ts
@@ -5,9 +5,32 @@ dotenv.config();
 import discord from 'discord.js';
 const discordClient = new discord.Client();
 
+// Import types
+import Message from './types/message';
+
+// Import models
+import User from './models/User';
+
 discordClient.once('ready', () => {
     // tslint:disable-next-line:no-console
     console.log('Bot Ready!');
+});
+
+discordClient.on('message', (message: discord.Message) => {
+    if (!message.author.bot) {
+        const data: Message = JSON.parse(message.content);
+        // Send the message to the given user
+        message.client.users.fetch(data.to)
+            .then(recipient => {
+                recipient.send(JSON.stringify({
+                    content: data.content,
+                    from: message.author.id
+                }));
+            })
+            .catch(reason => {
+                message.channel.send(reason);
+            });
+    }
 });
 
 export default discordClient;
