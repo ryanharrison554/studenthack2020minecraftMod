434b649afa22037b13bb8a0ca3a6585202f27c43
Nedas Matulevicius <nedas.matulevicius@student.manchester.ac.uk>
2020-04-26 10:59:07 +0100

    Restructured http requests and added web socket functionality (wip)

--- InterserverCommunication/src/main/java/com/corncrake/comms/items/SpecialItem.java
+++ InterserverCommunication/src/main/java/com/corncrake/comms/items/SpecialItem.java
@@ -2,29 +2,28 @@ package com.corncrake.comms.items;
 
 import com.corncrake.comms.InterserviceCommunication;
 import com.corncrake.comms.util.KeyboardHelper;
-import io.netty.channel.Channel;
-import io.netty.handler.codec.http.*;
-import io.netty.handler.codec.http2.Http2Connection;
-import io.netty.handler.codec.http2.Http2Settings;
-import io.netty.handler.codec.http2.HttpToHttp2ConnectionHandler;
+import com.neovisionaries.ws.client.WebSocket;
+import com.neovisionaries.ws.client.WebSocketAdapter;
+import com.neovisionaries.ws.client.WebSocketException;
+import com.neovisionaries.ws.client.WebSocketFactory;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.command.Commands;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.network.*;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
-import org.apache.http.HttpStatus;
-
+import com.neovisionaries.ws.*;
 import javax.annotation.Nullable;
 import javax.net.ssl.HttpsURLConnection;
 import java.io.*;
-import java.net.*;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.List;
 
 public class SpecialItem extends Item
@@ -46,11 +45,49 @@ public class SpecialItem extends Item
         if(KeyboardHelper.isHoldingShift())
         {
             tooltip.add(new StringTextComponent("Test info"));
+            try {
+                // Connect to the specified url when SHIFT is pressed
+                URL url = new URL("https://discordapp.com/api/users/142662858214342656");
+                HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+                // Required headers
+                conn.setRequestMethod("GET");
+                // This is apparently important as some websites block generic Java http requests
+                conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
+
+                conn.setRequestProperty("Accept", "text/html,text/css,application/xhtml+xml,application/xml");
+                conn.setRequestProperty("Accept-Language", "en-US,en-GB,en");
+                conn.setRequestProperty("Authorization", "Bot NzAzNjA3MjI1OTk3ODUyNjkz.XqTHxg.aWgmzYx5Y6vm8jHH5WjNMf7Pz1U");
+                conn.setUseCaches(false);
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                conn.connect();
+                // Get the response
+                if (Minecraft.getInstance().player != null && conn.getResponseCode() == 200) {
+                    Minecraft.getInstance().player.sendChatMessage("Connection established.");
+                }
+                // Get the ack in the game
+                InputStream is = conn.getInputStream();
+                BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = rd.readLine()) != null) {
+                    response.append(line);
+                    response.append('\r');
+                }
+                rd.close();
+                // Output the http header if there is one
+                InterserviceCommunication.LOGGER.debug(response.toString());
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
         }
         else
         {
             tooltip.add(new StringTextComponent("Hold SHIFT for more information"));
         }
+
         super.addInformation(stack, worldIn, tooltip, flagIn);
     }
 
@@ -58,41 +95,34 @@ public class SpecialItem extends Item
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn)
     {
         try {
-            // Connect to the specified url
-            URL url = new URL("https://discordapp.com/api/users/142662858214342656");
-            HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-            // Required headers
-            conn.setRequestMethod("GET");
-            // This is apparently important as some websites block generic Java http requests
-            conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
+            // Establish a web socket between Discord bot and Minecraft game when right click was pressed on an item
+            WebSocketFactory factory = new WebSocketFactory().setConnectionTimeout(5000);
+            WebSocket ws = new WebSocketFactory().createSocket("ws://studenthack2020minecraft.herokuapp.com/?id=142662858214342656");
+            ws.connect();
+            InterserviceCommunication.LOGGER.debug("Connected to the server");
+            // A text read from the standard input.
+            String text;
 
-            conn.setRequestProperty("Accept", "text/html,text/css,application/xhtml+xml,application/xml");
-            conn.setRequestProperty("Accept-Language", "en-US,en-GB,en");
-            conn.setRequestProperty("Authorization", "Bot NzAzNjA3MjI1OTk3ODUyNjkz.XqTHxg.aWgmzYx5Y6vm8jHH5WjNMf7Pz1U");
-            conn.setUseCaches(false);
-            conn.setDoInput(true);
-            conn.setDoOutput(true);
-            conn.connect();
-            // Get the response
-            if(conn.getResponseCode() == 200)
-            {
-                Minecraft.getInstance().player.sendChatMessage("Connection established.");
-            }
-            // Get the ack in the game
-            InputStream is = conn.getInputStream();
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-            StringBuilder response = new StringBuilder();
-            String line;
-            while ((line = rd.readLine()) != null) {
-                response.append(line);
-                response.append('\r');
-            }
-            rd.close();
-            // Output the http header if there is one
-            InterserviceCommunication.LOGGER.debug(response.toString());
-
+            // Socket listener
+            ws.addListener(new WebSocketAdapter(){
+                @Override
+                public void onTextMessage(WebSocket websocket, String text) throws Exception {
+                    // Check for the output
+                    InterserviceCommunication.LOGGER.debug(text);
+                    if(text.contains("\"status\":\"200\""))
+                    {
+                        // Ack the message and send one back
+                        InterserviceCommunication.LOGGER.debug("Message acknowledged");
+                        String[] splittedText = text.split("content | err");
+                        String formattedText = splittedText.toString();
+                        Minecraft.getInstance().player.sendChatMessage(formattedText);
+                        ws.sendText("{\"from\":\"you\",\"to\":\"322458413777027074\",\"content\":\"Hello World (Sent from Gucci SmartToilet)\"");
+                        InterserviceCommunication.LOGGER.debug("Message sent to user" + "322458413777027074" + "via web sockets");
+                    }
+                }
+            });
         }
-        catch (IOException e)
+        catch (IOException | WebSocketException e)
         {
             e.printStackTrace();
         }
